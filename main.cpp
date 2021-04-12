025eafe52c8b6ff5240007218980f8470a064f90
jansawiris <78458236+jansawiris@users.noreply.github.com>
2021-04-12 18:09:13 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include<iostream>
 #include"tinyxml2.h"
 #include<iomanip>
+#include<string.h>
 using namespace std;
 using namespace tinyxml2;
 
 
 /*
 
-	We are going to do two things here in this code
+	We are doing two things here in sequence
 		1) Read the records from the XMl file 
-		2) Write records into an xml file
-			
+		2) Write two records into an xml file
+			-> One for Craig
+			-> one for Roy
 		2) Read the records from the XMl file after writing to check if the account is written into the file
 
 	tinyXMl2 treats each entry of the XML document as an XML element 
@@ -23,66 +25,77 @@ using namespace tinyxml2;
 */
 
 
-
-
-
-
 int main() {
+
+	// Create an XML Document
 	XMLDocument doc;
 	const char * path = "Accounts.xml";
 	char amountText[100];
+
 	// Load the XML file into the Doc instance
 	doc.LoadFile(path);
-	// Get root Element
+// Get root Element, root element is the first XML element or the first record in the file
 	XMLElement * pRootElement = doc.RootElement();
-	// Reading
-	cout << '|' << setw(7) << "Reading From File Accounts.xml .........." << '|' << endl;
+	
+	// Read the XML and print the results in a tabluar format
+	// Note: setw(7) will print the string with fixed with of 7 characters wide 
+
+cout << '|' << setw(7) << "Reading From File Accounts.xml .........." << '|' << endl;
 	// Print out the Table Header
-	cout << '|' << setw(7) << "Acc No." << '|' << setw(12) << "Account Type" << '|' << setw(15) << "Customer Name" << '|' << setw(10) << "Balance" << '|' << setw(12) << "Open Date" <<
+cout << '|' << setw(7) << "Acc No." << '|' << setw(12) << "Account Type" << '|' << setw(15) << "Customer Name" << '|' << setw(10) << "Balance" << '|' << setw(12) << "Open Date" <<
 		endl << endl;
+
+       // 
 	if (NULL != pRootElement) {
+
 		//Get 'Accounts' Child
-		XMLElement * pAccounts = pRootElement->FirstChildElement("Accounts");
+XMLElement * pAccounts = pRootElement->FirstChildElement("Accounts");
 		if (NULL != pAccounts) {
+
+
 			//Get 'Account' Child
-			XMLElement * pAccount = pAccounts->FirstChildElement("Account");
+	XMLElement * pAccount = pAccounts->FirstChildElement("Account");
+			
+			// Loop through all existing 
 			while (pAccount) {
-				// Get 'AccountNo' Child
-				XMLElement * pAccountNo = pAccount->FirstChildElement("AccountNo");
+
+			// Get 'AccountNo' Child
+	XMLElement * pAccountNo = pAccount->FirstChildElement("AccountNo");
 				if (NULL != pAccountNo) {
-					//Print out AccountNo
-					cout << '|' << setw(7) << pAccountNo->GetText();
+			//Print out AccountNo
+			cout << '|' << setw(7) << pAccountNo->GetText();
 				}
 				//Get 'type' Child
-				XMLElement * pType = pAccount->FirstChildElement("type");
+		XMLElement * pType = pAccount->FirstChildElement("type");
 				if (NULL != pType) {
 					// Print out 'Tyoe'
-					cout << '|' << setw(12) << pType->GetText();
+			cout << '|' << setw(12) << pType->GetText();
 				}
 				// Get 'customer' Child
-				XMLElement * name = pAccount->FirstChildElement("customer");
-				if (NULL != name) {
+	XMLElement * pname = pAccount->FirstChildElement("customer");
+				if (NULL != pname) {
 					//Print out name
-					cout << '|' << setw(15) << name->GetText();
+				cout << '|' << setw(15) << pname->GetText();
 				}
 				// Get 'balance' Child
-				XMLElement * balance = pAccount->FirstChildElement("balance");
-				if (NULL != balance) {
-					// Convert to *char
-					strcpy(amountText, "$");
-					strcat(amountText, balance->GetText());
+	XMLElement * pbalance = pAccount->FirstChildElement("balance");
+				if (NULL != pbalance) {
+			
 					//Print out amount
-					cout << '|' << setw(10) << amountText << '|';
+		cout << '|' << setw(10) <<"$"<< pbalance->GetText() << '|';
 				}
 				// Get 'openDate' Child
-				XMLElement * openDate = pAccount->FirstChildElement("openDate");
-				if (NULL != openDate) {
+	XMLElement * popenDate = pAccount->FirstChildElement("openDate");
+				if (NULL != popenDate) {
 					//Print out openDate
-					cout << setw(12) << openDate->GetText() << '|';
+			cout << setw(12) << popenDate->GetText() << '|';
 				}
 				cout << endl;
-				return 0;
+cout << "------------------------------------------------------------";
+				std::cout << std::endl;
+				// Next Account
+			pAccount = pAccount->NextSiblingElement("Account");
+			}
+			cout << "\n";
 		}
 	}
-}
-}
